94e3ac388872f661c4a96169acf27de16d640182
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-20 19:17:55 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -9,16 +9,22 @@ import random
 
 class React(Cog_extension):
     
-    @commands.command()
-    async def imege(self, ctx):   #指定圖片
+    @commands.command()     #指定圖片
+    async def imege(self, ctx):     #PATH
         pic = discord.File(setting["Imege"])
         await ctx.send(file = pic)
 
-    @commands.command()
-    async def logo(self, ctx):    #隨機圖片
+    @commands.command()     #隨機圖片
+    async def logo(self, ctx):      #PATH
         random_pic = random.choice(setting["Logo"])
         pic = discord.File(random_pic)
         await ctx.send(file = pic)
+
+    @commands.command()     #抽籤
+    async def omikuji(self, ctx):     #URL
+        random_pic = random.choice(setting["Omikuji"])
+        await ctx.send(random_pic)
+
 
 async def setup(bot):
     await bot.add_cog(React(bot))
