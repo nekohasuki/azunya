6f31c1b12806149b6ce8c44b8156e41435443e16
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-20 19:17:42 +0800

    Update azunya.py

--- azunya.py
+++ azunya.py
@@ -9,19 +9,36 @@ import random
 #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix =["a-","/",""], intents = intents)
-@bot.event
+@bot.event  #機器人登陸通知(僅後台)
 async def on_ready():
     print(">>","嗨嗨嗨!! {0.user}已經成功登陸嘍!!!".format(bot),"<<")
 
-@bot.event
+@bot.event  #成員加入通知
 async def on_member_join(member):
+    print(f'User {member} 加入了伺服器!')
     channel = bot.get_channel(int(setting["Welcome_channel"]))
-    await channel.send(f"{member} join!")
-    
-@bot.event
+    await channel.send(f'User** {member} **加入了伺服器!')
+
+@bot.event  #成員退出通知
 async def on_member_remove(member):
+    print(f'User {member} 離開了伺服器!')
     channel = bot.get_channel(int(setting["Welcome_channel"]))
-    await channel.send(f"{member} leave!")
+    await channel.send(f'User** {member} **離開了伺服器!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 import asyncio
