504cbc878fcbea998bdf2f5258ece2126400c414
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-20 15:52:48 +0800

    Update azunya.py

--- azunya.py
+++ azunya.py
@@ -1,8 +1,17 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix =["a-","/"])
+import json
+with open("setting.json","r",encoding="utf8") as setting_file:
+    setting = json.load(setting_file)
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix =["a-","/"], intents = intents)
 
 @bot.event
 async def on_ready():
     print(">>","嗨嗨嗨!! {0.user}已經成功登陸嘍!!!".format(bot),"<<")
+
+
+bot.run(setting["TOKEN"])
