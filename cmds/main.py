8dc4a7eeef79cf5d87946f38bb6e0ceabfe804ba
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-20 17:28:52 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -1,2 +1,12 @@
 import discord
 from discord.ext import commands
+from core.classes import Cog_extension
+
+class Main(Cog_Exception):
+        
+    @commands.command()
+    async def ping(self, ctx):    #延遲
+        await ctx.send(f"{round(self.bot.latency*1000)} (ms)")
+
+def setup(bot):
+    bot.add_cog(Main(bot))
