1313cffcbb57144f8c499ee349bd8a81f06fa223
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-20 18:14:40 +0800

    Update azunya.py

--- azunya.py
+++ azunya.py
@@ -1,20 +1,17 @@
 import discord
 from discord.ext import commands
-
+import os
 
 import json
 with open("setting.json","r",encoding="utf8") as setting_file:
     setting = json.load(setting_file)
-    
+import random
 #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix =["a-","/",""], intents = intents)
 @bot.event
 async def on_ready():
     print(">>","嗨嗨嗨!! {0.user}已經成功登陸嘍!!!".format(bot),"<<")
-
-
 
 @bot.event
 async def on_member_join(member):
@@ -25,20 +22,13 @@ async def on_member_join(member):
 async def on_member_remove(member):
     channel = bot.get_channel(int(setting["Welcome_channel"]))
     await channel.send(f"{member} leave!")
-
-@bot.command()
-async def ping(ctx):    #延遲
-    await ctx.send(f"{round(bot.latency*1000)} (ms)")
     
 
-@bot.command()
-async def imege(ctx):   #指定圖片
-    pic = discord.File(setting["Imege"])
-    await ctx.send(file = pic)
-
-
-
-
-
-
-bot.run(setting["TOKEN"])
+import asyncio
+async def main():
+    for Filename in  os.listdir("./cmds"):
+        if Filename.endswith("py"):
+            await bot.load_extension(f"cmds.{Filename[:-3]}")
+    await bot.start(setting["TOKEN"])
+if __name__=="__main__":
+    asyncio.run(main())
