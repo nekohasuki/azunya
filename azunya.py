c8aafc6558f4b1b8ef2663a3bdaed34319fc13bc
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-20 16:50:56 +0800

    Update azunya.py

--- azunya.py
+++ azunya.py
@@ -12,24 +12,28 @@ with open("setting.json","r",encoding="utf8") as setting_file:
 
 #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix =["a-","/"], intents = intents)
+bot = commands.Bot(command_prefix =["a-","/",""], intents = intents)
 @bot.event
 async def on_ready():
     print(">>","嗨嗨嗨!! {0.user}已經成功登陸嘍!!!".format(bot),"<<")
 
+
+
+
 @bot.event
 async def on_member_join(member):
-    print(f"{member} join!")
+    channel = bot.get_channel(int(setting["Welcome_channel"]))
+    await channel.send(f"{member} join!")
     
 @bot.event
 async def on_member_remove(member):
-    print(f"{member} leave!")
+    channel = bot.get_channel(int(setting["Welcome_channel"]))
+    await channel.send(f"{member} leave!")
 
-
-
-
-
-
+@bot.command()
+async def ping(ctx):    #延遲
+    await ctx.send(f"{round(bot.latency*1000)}/ms")
+    
 
 
 
