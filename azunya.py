098f998c724779816f0764e402ab0dddccd1cb43
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-20 16:34:06 +0800

    Create azunya.py

---
+++ azunya.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+
+
+import json
+with open("setting.json","r",encoding="utf8") as setting_file:
+    setting = json.load(setting_file)
+
+
+
+
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix =["a-","/"], intents = intents)
+@bot.event
+async def on_ready():
+    print(">>","嗨嗨嗨!! {0.user}已經成功登陸嘍!!!".format(bot),"<<")
+
+@bot.event
+async def on_member_join(member):
+    print(f"{member} join!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+bot.run(setting["TOKEN"])
