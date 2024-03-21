86f05992ef195d2e63e785b9fef0185a155b5895
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-21 16:01:22 +0800

    Update main.py

--- cmds/main.py
+++ cmds/main.py
@@ -8,6 +8,17 @@ class Main(Cog_extension):
     async def ping(self, ctx):      #延遲
         await ctx.send(f"{round(self.bot.latency)}/s\n"  f"{round(((self.bot.latency)-round(self.bot.latency))*1000)}/ms"),
 
+    @commands.command()
+    async def say(self, ctx, *,msg):
+        await ctx.message.delete()
+        await ctx.send(msg)
+
+    @commands.command()
+    async def clear(self, ctx, num:int):
+        member = self.bot.get_user(id(int))
+        deleted = await ctx.channel.purge(limit=num+1)
+        await ctx.channel.send(f'已為USER:{member}刪除{len(deleted)-1}條訊息')
+
 
 async def setup(bot):
     await bot.add_cog(Main(bot))
