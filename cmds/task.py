24e97619fc11e02fd5a48dff9a3193aef4fdcb94
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-21 19:22:39 +0800

    Update task.py

--- cmds/task.py
+++ cmds/task.py
@@ -1,11 +1,11 @@
 import discord
 from discord.ext import commands
 from core.classes import Cog_extension
-import json, asyncio, datetime
+import json,asyncio,datetime
 
 class Task(Cog_extension):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
 
 #        async def interval():
 #            await self.bot.wait_until_ready()
