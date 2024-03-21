8e70860a77f4c99bc88e84de79053ff3104d8691
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-22 02:40:20 +0800

    Update azunya.py

--- azunya.py
+++ azunya.py
@@ -49,5 +49,10 @@ async def main():
             await bot.load_extension(f"cmds.{Filename[:-3]}")
     await bot.start(setting["TOKEN"])
 if __name__=="__main__":
-    #keep_alive.keep_alive()
-    asyncio.run(main())
+    if setting["keep_alive"] == "True":
+        keep_alive.keep_alive()
+        asyncio.run(main())
+    elif setting["keep_alive"] == "False":
+        asyncio.run(main())
+    else:
+        print("ERROR")
