37433ad896f367cccdea7dfeebd358b4ceff9334
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-21 19:21:41 +0800

    Update azunya.py

--- azunya.py
+++ azunya.py
@@ -5,27 +5,26 @@ import os
 import json
 with open("setting.json","r",encoding="utf8") as setting_file:
     setting = json.load(setting_file)
-import asyncio, random
-#import keep_alive
+import asyncio,random,keep_alive
 #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix =["a-","/",""], intents = intents)
+bot = commands.Bot(command_prefix =["a-","/",""],intents = intents)
 @bot.event  #機器人登陸通知(僅後台)
 async def on_ready():
     print(">>","嗨嗨嗨!! {0.user}已經成功登陸嘍!!!".format(bot),"<<")
 
 @bot.command()      #加載類別
-async def load(ctx, extension):
+async def load(ctx,extension):
     await bot.load_extension(f"cmds.{extension}")
     await ctx.send(f"Loaded {extension} done.")
 
 @bot.command()      #重新加載類別
-async def reload(ctx, extension):
+async def reload(ctx,extension):
     await bot.reload_extension(f"cmds.{extension}")
     await ctx.send(f"Reloaded {extension} done.")
 
 @bot.command()      #取消加載類別
-async def unload(ctx, extension):
+async def unload(ctx,extension):
     await bot.unload_extension(f"cmds.{extension}")
     await ctx.send(f"Unloaded {extension} done.")
 
@@ -41,5 +40,5 @@ async def main():
             await bot.load_extension(f"cmds.{Filename[:-3]}")
     await bot.start(setting["TOKEN"])
 if __name__=="__main__":
-#    keep_alive.keep_alive()
+    #keep_alive.keep_alive()
     asyncio.run(main())
