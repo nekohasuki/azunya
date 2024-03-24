e28497e1793d68ac7f19b1fb955add1b03099ff4
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-24 22:41:51 +0800

    Update classes.py

--- core/classes.py
+++ core/classes.py
@@ -1,6 +1,17 @@
 import discord
 from discord.ext import commands
+import datetime
 
 class Cog_extension(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
+
+class Logger:
+    def log(self, ctx, data, type='error'):
+        '''事件紀錄器'''
+        time = datetime.datetime.now().strftime('[%Y-%m-%d] [%H:%M]')
+        user = ctx.author.name
+        channel = ctx.channel.name
+        command = ctx.command
+        if type == 'error':
+            print(f'<Error Log>: {time}/[{user}][{channel}][{command}]: {data}')
