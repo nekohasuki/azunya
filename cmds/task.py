d5545cca645316fff544eda7ceb7d3eb4b69fdf2
nekohasuki <nekohasuki.ch@gmail.com>
2024-03-24 10:14:41 +0800

    Update task.py

--- cmds/task.py
+++ cmds/task.py
@@ -9,13 +9,21 @@ class Task(Cog_extension):
 
 #        async def interval():
 #            await self.bot.wait_until_ready()
-#            self.channel = self.bot.get_channel(1219815148622057523)
+#            self.channel = self.bot.get_channel(1221262279484510310)
 #            while not self.bot.is_closed():
 #                await self.channel.send("Hi i'm running")
-#                await asyncio.sleep(5)#5/s
-#
+#                await asyncio.sleep(5)#單位:秒
 #        self.bg_task = self.bot.loop.create_task(interval())
-           
-        
+#    @commands.command()
+#    async def setchannel(self,ctx,channel_id:int):
+#        self.channel = self.bot.get_channel(channel_id)
+#        await ctx.sand(f"Set channel:{self.channel.mention}")
+
+
+
+
+
+
+
 async def setup(bot):
     await bot.add_cog(Task(bot))
