11665af34e4fd09ff4bf0704c8cc648bd734c118
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-01 05:36:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,37 @@
 import datetime
 Current_Time = datetime.datetime.now().strftime("%H:%M")
-print(Current_Time)
+# print(Current_Time)
+# @commands.command()
+# async def omikuji(self, ctx):
+#     user = ctx.get_user()
+#     if Current_Time != ("18:00"):
+#         if "history.json" == 有紀錄:
+#             await ctx.send(f"User : {user}\n你今天已經抽過了啦!\n[點我看抽到的籤]({對應訊息網址})")
+#         else:
+#             random_pic = random.choice(setting["Omikuji"])
+#             await ctx.send(random_pic)
+#             將 User : 對應訊息網址 存入"history.json"
+#     else:
+#         await ctx.send("系統維護中，請稍等1分鐘")
+
+
+# Clock = 0
+# if Current_Time == ("18:00"):
+#     print(Clock)
+#     Clock == 1
+#     print(Clock)
+# if Current_Time == ("18:01") and Clock == 0:
+#     # 清空 history.json
+#     print(Clock)
+#     Clock += 1
+#     print(Clock)
 
 
 
 
-@commands.command()
-async def omikuji(self, ctx):
-    user = ctx.get_user()
-    if Current_Time != ("18:00"):
-        if "history.json" == 有紀錄:
-            await ctx.send(f"User : {user}\n你今天已經抽過了啦!\n[點我看抽到的籤]({對應訊息網址})")
-        else:
-            random_pic = random.choice(setting["Omikuji"])
-            await ctx.send(random_pic)
-            將 User : 對應訊息網址 存入"history.json"
-    else:
-        await ctx.send("系統維護中，請稍等1分鐘")
 
-
-Clock = 0
-if Current_Time == ("18:00"):
-    print(Clock)
-    Clock == 1
-    print(Clock)
-if Current_Time == ("18:01") and Clock == 0:
-    # 清空 history.json
-    print(Clock)
-    Clock += 1
-    print(Clock)
-
-
-
+# {
+#     "userdata": [697842681082281985],
+#     "697842681082281985": "https://www.illust-box.jp/db_img/sozai/00019/196972/watermark.jpg"
+    
+# }
