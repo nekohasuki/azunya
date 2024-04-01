e20680c766d796542db3b14b53b6c350e84e1e52
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-02 05:03:13 +0800

    Update test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import json
-with open("cmds\data\omikuji.json","r",encoding="utf8") as omikuji_file:
-    omikuji = json.load(omikuji_file)
-    omikuji={"userdata": [],"namedata": []}
-    omikuji.update(omikuji)
-with open("cmds\data\omikuji.json","w",encoding="utf8") as omikuji_file:
-    json.dump(omikuji,omikuji_file)
