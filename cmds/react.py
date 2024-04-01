e09307a8497bd73189b5e783d8e63bc6408ed55b
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-01 17:27:25 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -33,43 +33,30 @@ class React(Cog_extension):
             # channel = ctx.channel
             user = ctx.author.id
             name = ctx.author
-            #如果抽過了就回傳抽出結果
-            if user in omikuji["userdata"]:
-                await ctx.send(f"[User :]({omikuji[f"{int(user)}"]}) <@{user}>\n你今天已經抽過了啦!")
-            #沒抽過就抽出結果後更新資料進"omikuji.json"
+            if Current_Time == setting["OmikujiTime"]:
+                #如果抽過了就回傳抽出結果
+                if user in omikuji["userdata"]:
+                    await ctx.send(f"[User :]({omikuji[f"{int(user)}"]}) <@{user}>\n你今天已經抽過了啦!")
+                #沒抽過就抽出結果後更新資料進"omikuji.json"
+                else:
+                    random_pic = random.choice(setting["Omikuji"])
+                    await ctx.send("抽出的結果是!!!!\n(搖籤筒聲)")
+                    await asyncio.sleep (3)
+                    await ctx.send(f"[User :]({random_pic}) <@{user}>\n抽出抽出結果了!!快看快看!!!")
+                    #資料更新
+                    usercache.append (user) 
+                    namecache.append({f"{user}":f"{name}"})
+                    omikuji_update = {"namedata":namecache,"userdata":usercache,f"{user}":random_pic}
+                    omikuji.update(omikuji_update)
+                    with open("cmds\data\omikuji.json","w",encoding="utf8") as omikuji_file:
+                        json.dump(omikuji,omikuji_file,indent=4)
             else:
-                random_pic = random.choice(setting["Omikuji"])
-                await ctx.send("抽出的結果是!!!!\n(搖籤筒聲)")
-                await asyncio.sleep (3)
-                await ctx.send(f"[User :]({random_pic}) <@{user}>\n抽出抽出結果了!!快看快看!!!")
-                #資料更新
-                usercache.append (user) 
-                namecache.append({f"{user}":f"{name}"})
-                omikuji_update = {"namedata":namecache,"userdata":usercache,f"{user}":random_pic}
-                omikuji.update(omikuji_update)
-                with open("cmds\data\omikuji.json","w",encoding="utf8") as omikuji_file:
-                    json.dump(omikuji,omikuji_file,indent=4)
+                await ctx.send("系統維護中，請稍等1分鐘")
     #取得用戶ID
     @commands.command()
     async def myid(self,ctx):
         id = ctx.author.id
         await ctx.send(f'User：<@{id}>的ID是__{id}__呦!')
-
-
-
-
-# {
-#     "userdata":[697842681082281985],
-#     "697842681082281985":"https://www.illust-box.jp/db_img/sozai/00019/196972/watermark.jpg"
-# }
-
-
-
-
-
-
-
-
 
 
 
