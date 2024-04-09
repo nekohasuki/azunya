58bb71ab94f77bf6db1c2a6971eee2c980a386df
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-09 17:14:04 +0800

    Update azunya.py

--- azunya.py
+++ azunya.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 intents = discord.Intents.all()
 intents.typing = False
 intents.presences = False
-bot = commands.Bot(command_prefix =["a-","/"],intents = intents)
-
 import json
 with open("setting.json","r",encoding="utf8") as setting_file:
     setting = json.load(setting_file)
-import os,asyncio,random,keep_alive
+bot = commands.Bot(command_prefix =[f"{setting["prefix"]}-","/"],intents = intents)
+
+import asyncio,keep_alive,os
 from typing import Optional
 
 #機器人登陸通知
@@ -21,62 +21,62 @@ async def on_ready():
     except Exception as e:
         print("命令同步時發生錯誤: ", e)
 #加載類別
-@bot.hybrid_command(name='load', help='load extension')
-async def load(ctx,extension: Optional[str] = None):
-    user = ctx.author.id
+@bot.tree.command(name='load', description='加載擴充類別')
+async def load(interaction: discord.Interaction,extension: Optional[str] = None):
+    user = interaction.user.id
     extension_list = []
     for allfile in os.listdir("./cmds"):
         if allfile.endswith(".py"):extension_list.append(allfile[:-3])
     if extension == None:
-        await ctx.send(f"請輸入需要加載類別\n如果User：<@{user}>不知道目前類別有哪些\n請參考：\n{extension_list}")
+        await interaction.response.send_message(f"請輸入需要加載類別\n如果User：<@{user}>不知道目前類別有哪些\n請參考：\n{extension_list}")
     elif extension == "all":
         for reload in extension_list:
             await bot.load_extension(f"cmds.{reload}")
-        await ctx.send(f"已成功加載__{len(extension_list)}__項擴充類別")
+        await interaction.response.send_message(f"已成功加載__{len(extension_list)}__項擴充類別")
     else:
         if extension in extension_list:
             await bot.load_extension(f"cmds.{extension}")
-            await ctx.send(f"已成功加載擴充類別：__{extension}__")
+            await interaction.response.send_message(f"已成功加載擴充類別：__{extension}__")
         else:
-            await ctx.send(f"沒有擴充類別：'{extension}'")
+            await interaction.response.send_message(f"沒有擴充類別：'{extension}'")
 #重新加載類別
-@bot.hybrid_command(name='reload', help='reload extension')
-async def reload(ctx,extension: Optional[str] = None):
-    user = ctx.author.id
+@bot.tree.command(name='reload', description='重新加載擴充類別')
+async def reload(interaction: discord.Interaction,extension: Optional[str] = None):
+    user = interaction.user.id
     extension_list = []
     for allfile in os.listdir("./cmds"):
         if allfile.endswith(".py"):extension_list.append(allfile[:-3])
     if extension == None:
-        await ctx.send(f"請輸入需要重新加載類別\n如果User：<@{user}>不知道目前類別有哪些\n請參考：\n{extension_list}")
+        await interaction.response.send_message(f"請輸入需要重新加載類別\n如果User：<@{user}>不知道目前類別有哪些\n請參考：\n{extension_list}")
     elif extension == "all":
         for reload in extension_list:
             await bot.reload_extension(f"cmds.{reload}")
-        await ctx.send(f"已成功重新加載__{len(extension_list)}__項擴充類別")
+        await interaction.response.send_message(f"已成功重新加載__{len(extension_list)}__項擴充類別")
     else:
         if extension in extension_list:
             await bot.reload_extension(f"cmds.{extension}")
-            await ctx.send(f"已成功重新加載擴充類別：__{extension}__")
+            await interaction.response.send_message(f"已成功重新加載擴充類別：__{extension}__")
         else:
-            await ctx.send(f"沒有擴充類別：'{extension}'")
+            await interaction.response.send_message(f"沒有擴充類別：'{extension}'")
 #取消加載類別
-@bot.hybrid_command(name='unload', help='unload extension')
-async def unload(ctx,extension: Optional[str] = None):
-    user = ctx.author.id
+@bot.tree.command(name='unload', description='取消加載擴充類別')
+async def unload(interaction: discord.Interaction,extension: Optional[str] = None):
+    user = interaction.user.id
     extension_list = []
     for allfile in os.listdir("./cmds"):
         if allfile.endswith(".py"):extension_list.append(allfile[:-3])
     if extension == None:
-        await ctx.send(f"請輸入需要取消加載類別\n如果User：<@{user}>不知道目前類別有哪些\n請參考：\n{extension_list}")
+        await interaction.response.send_message(f"請輸入需要取消加載類別\n如果User：<@{user}>不知道目前類別有哪些\n請參考：\n{extension_list}")
     elif extension == "all":
         for reload in extension_list:
             await bot.unload_extension(f"cmds.{reload}")
-        await ctx.send(f"已成功取消加載__{len(extension_list)}__項擴充類別")
+        await interaction.response.send_message(f"已成功取消加載__{len(extension_list)}__項擴充類別")
     else:
         if extension in extension_list:
             await bot.unload_extension(f"cmds.{extension}")
-            await ctx.send(f"已成功取消重新加載擴充類別：__{extension}__")
+            await interaction.response.send_message(f"已成功取消重新加載擴充類別：__{extension}__")
         else:
-            await ctx.send(f"沒有擴充類別：'{extension}'")
+            await interaction.response.send_message(f"沒有擴充類別：'{extension}'")
 
 
 
