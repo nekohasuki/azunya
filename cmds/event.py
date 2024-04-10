55d54be46b01ab4b829549242e8a4a938d5fbb36
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-10 16:54:18 +0800

    Update event.py

--- cmds/event.py
+++ cmds/event.py
@@ -97,13 +97,27 @@ class Event(Cog_extension):
         #         await user.remove_roles(role) 
 #偵測到任一訊息時...
     @commands.Cog.listener()
-    async def on_message(self,msg):
-        print(msg)
+    async def on_message(self,ctx):
         with open("setting.json","r",encoding="utf8") as setting_file:
             setting = json.load(setting_file)
+        guild = ctx.guild
+        channel = ctx.channel
+        name = ctx.author.mention
+        user = ctx.author.id
+        msg = ctx.content
+        log_channel = self.bot.get_channel(int(setting["LOG_CHANNEL_ID"]))
+        nowtime = datetime.datetime.now().strftime("%H:%M:%S")
+        if channel.id != log_channel.id and guild.id == int(setting["LOG_GUILD_ID"]):
+            await log_channel.send(f"{nowtime}\n**[ {guild} ]**　|　{channel}\n{name.mention}(`ID:{user}`)：\n{msg}[`訊息連結`](https://discord.com/channels/{guild.id}/{channel.id}/{ctx.id})")
+        #字典
         dict_my = dict["user_self-proclaimed"]
         dict_azunya = dict["azunya"]
         dict_omikuji = dict["omikuji"]
+        dict_food = dict["food"]
+        dict_morning = dict["morning"]
+        dict_afternoon = dict["afternoon"]
+        dict_evening = dict["evening"]
+        dict_night = dict["night"]
         #==:等於/!=:不等於
         #in:等於/not in:不等於
         #endswith:結束詞/startswith:開始詞
@@ -111,14 +125,66 @@ class Event(Cog_extension):
         #and:以及/or:
 
         #……以及訊息不為自身(機器人)發出
-        #<and msg.author != self.bot.user:>
+        #<and ctx.author != self.bot.user:>
         #……以及訊息中如果有包含關鍵字(key)
-        #<and any(word in msg.content for word in key)>
+        #<and any(word in ctx.content for word in key)>
         #……以及訊息等於關鍵字(key)
-        #<and msg.content == key>
+        #<and ctx.content == key>
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        #問候
+        if any(word in ctx.content for word in dict_azunya):
+            #早上
+            if any(word in ctx.content for word in dict_morning):
+                if nowtime > "06:00:00" and nowtime < "11:00:00":
+                    await ctx.channel.send(f"{name}也早安(≧ω≦)/\n{name}今天抽籤了嗎?")
+                else:
+                    await ctx.channel.send(f"早...安?")
+            #中午~下午
+            elif any(word in ctx.content for word in dict_afternoon):
+                if nowtime > "11:00:00" and nowtime < "13:00:00":
+                    food = random.choice(dict_food)
+                    await ctx.channel.send(f"{name}午餐吃什麼??\n順帶一題我剛吃了{food}")
+                elif nowtime > "13:00:00" and nowtime < "19:00:00":
+                    await ctx.channel.send(f"{name}下午好呀~~")
+                else:
+                    await ctx.channel.send("午...安?\n等等,我看一下時間")
+                    await asyncio.sleep(1)
+                    await ctx.channel.send(f"現在好像是{nowtime}ㄟ")  
+            #晚上                  
+            elif any(word in ctx.content for word in dict_evening):
+                if nowtime > "19:00:00" or nowtime < "03:00:00":
+                    await ctx.channel.send(f"{name}晚上好呀晚上好")
+                else:
+                    await ctx.channel.send(f"{name}現在時間{nowtime}\n這時間可能不算晚喔?")
+            #睡前
+            elif any(word in ctx.content for word in dict_night):
+                if nowtime > "18:00:00" and nowtime < "21:00:00":
+                    await ctx.channel.send(f"{name}晚安,是說好早睡")
+                elif nowtime > "21:00:00" or nowtime < "03:00:00":
+                    await ctx.channel.send(f"{name}晚安,祝好夢")
+                    await ctx.channel.send(f"{name}晚安,是說好早睡")
+                elif nowtime < "06:00:00":
+                    await ctx.channel.send(f"晚安?\n{name}也太晚睡了吧???")
+                else:
+                    await ctx.channel.send(f"蛤?晚安?")
+                    await asyncio.sleep(1)
+                    await ctx.channel.send(f"好吧...晚安...")
+            #單純安安
+            elif any(word in ctx.content for word in "安安"):
+                await ctx.channel.send(f"{name}安安呀 安安 安安")
+            #안녕하세요(你好)
+            elif any(word in ctx.content for word in "安ニャー") and any(word in ctx.content for word in "SAY") and any(word in ctx.content for word in "呦"):
+                await ctx.channel.send(f"{name}안녕하세요")
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
     #抽籤系統/URL
-        if any(word in msg.content for word in (dict_azunya)) and any(word in msg.content for word in (dict_my)) and any(word in msg.content for word in (dict_omikuji)):
+        if any(word in ctx.content for word in (dict_azunya)) and any(word in ctx.content for word in (dict_my)) and any(word in ctx.content for word in (dict_omikuji)):
             with open("setting.json","r",encoding="utf8") as setting_file:
                 setting = json.load(setting_file)
             with open("cmds\data\omikuji.json","r",encoding="utf8") as omikuji_file:
@@ -127,26 +193,22 @@ class Event(Cog_extension):
             Current_minutes = datetime.datetime.now().strftime("%M")
             usercache = omikuji["userdata"]
             namecache = omikuji["namedata"]
-            # guild = ctx.guild
-            # channel = ctx.channel
-            user = msg.author.id
-            name = msg.author
             #如果當前時間等同於" setting["OmikujiTime"] "的設定時間
             if (f"{int(Current_hours)}:{int(Current_minutes)}") == setting["OmikujiTime"]:
-                await msg.channel.send("系統維護中，請稍等1分鐘")
+                await ctx.channel.send("系統維護中，請稍等1分鐘")
             else:
                 #如果抽過了就回傳抽出結果
                 if user in omikuji["userdata"]:
                     # pic = discord.File(f"imege\omikuji\{omikuji[f"{int(user)}"]}")
                     pic = discord.File(f"{omikuji[f"{int(user)}"]}")
-                    await msg.channel.send(f"User : <@{user}>\n你今天已經抽過了啦!",file = pic)
+                    await ctx.channel.send(f"User : <@{user}>\n你今天已經抽過了啦!",file = pic)
                 #沒抽過就抽出結果後更新資料進"omikuji.json"
                 else:
                     random_pic = random.choice(os.listdir("./imege/omikuji"))
                     pic = discord.File(f"imege\omikuji\{random_pic}")
-                    await msg.channel.send("抽出的結果是!!!!\n(搖籤筒聲)")
+                    await ctx.channel.send("抽出的結果是!!!!\n(搖籤筒聲)")
                     await asyncio.sleep (3)
-                    await msg.channel.send(f"User :<@{user}>\n抽出抽出結果了!!快看快看!!!",file=pic)
+                    await ctx.channel.send(f"User :<@{user}>\n抽出抽出結果了!!快看快看!!!",file=pic)
                     #資料更新
                     usercache.append (user) 
                     namecache.append({f"{user}":f"{name}"})
@@ -154,12 +216,10 @@ class Event(Cog_extension):
                     omikuji.update(omikuji_update)
                     with open("cmds\data\omikuji.json","w",encoding="utf8") as omikuji_file:
                         json.dump(omikuji,omikuji_file,indent=4)
-#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    #對話文本
-        if "A" == "A":
-            pass
-#///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+        #不要用那個稱呼
+        if any(word in ctx.content for word in dict["???"] ) and ctx.author.bot == False:
+            await asyncio.sleep(1)
+            await ctx.channel.send(f"還有，{name}不要用那個稱呼叫我!!!\n我叫__梓守__!,或者也可以叫我__梓喵__")
 
 
 
