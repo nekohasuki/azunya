86c1c70b641bd81bcd04d4a855343a9458fcd5a0
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-11 00:50:32 +0800

    Update data.py

--- cmds/data.py
+++ cmds/data.py
@@ -14,6 +14,27 @@ class Data(Cog_extension):
     @app_commands.command(name = "ping", description = "查看延遲")
     async def ping(self, interaction: discord.Interaction):
         await interaction.response.send_message(f"{round(self.bot.latency)}/s\n"  f"{round(((self.bot.latency)-round(self.bot.latency))*1000)}/ms"),
+#查詢用戶ID
+    @app_commands.command(name = "myid", description = "查詢Discird的ID")
+    async def myid(self,interaction:discord.Integration):
+        user = interaction.user
+        # await interaction.response.send_message("好的!")
+        await interaction.response.send_message(f"好的!")
+        message = await interaction.channel.send(f"正在查詢User：{user.mention}的資料......")
+        await asyncio.sleep(3)
+        await message.edit(content=f"User：<@{user.id}>\n你的ID是__{user.id}__呦!")
+#查身分資料
+    @app_commands.command(name = "checkrole", description = "查身分資料")
+    @app_commands.describe(role = "想查的身分")
+    async def checkrole(self,interaction:discord.Integration,role:Optional[str] = None):
+        if role == None:
+            await interaction.response.send_message("請輸入需要刪除的訊息數量\n參考：\n```/checkrole @新身分```")
+        if "@" in str(f"{role}"):
+            if "&" in str(f"{role}"):
+                A = interaction.guild.get_role(int(f"{role[3:-1]}"))
+                await interaction.response.send_message(f"# color:\n   {A.color}\n# created_at:\n    {A.created_at}\n# id:\n   {A.id}\n# members:\n  {A.members}\n# permissions:\n    {A.permissions}")
+            else:
+                await interaction.response.send_message(f"這好像是某位User並不是身分組")
 
 
 
