1e9c3e7215f0040f09b22d7c9e9d2c9ffad32a44
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-10 17:59:42 +0800

    Update setting.py

--- cmds/setting.py
+++ cmds/setting.py
@@ -19,27 +19,36 @@ class Setting(Cog_extension):
     #     self.channel = self.bot.get_channel(channel)
     #     await ctx.send(f"set chammel:{self.channel.mention}")
 #設定抽籤系統更新時間
-    @app_commands.command(name = "setomikujitime", description = "設定抽籤系統更新時間(需要權限)")
+    @app_commands.command(name = "setomikujitime", description = "抽籤系統更新時間相關")
+    @app_commands.describe(mod = "模式")
     @app_commands.describe(hours="輸入小時",minutes="輸入分鐘")
-    @app_commands.checks.has_permissions (administrator=True)
-    async def setomikujitime(self,interaction: discord.Interaction,hours: Optional[int] = None,minutes: Optional[int] = None):
-        if hours == None:
-            hours = 0
-        elif hours >= 23:
-            hours = 23
-        if minutes == None:
-            minutes = 0
-        elif minutes >= 59 :
-            minutes = 59
-        oldtime = setting["OmikujiTime"]
-        setting["OmikujiTime"] = f"{hours}:{minutes}"
-        newtime = setting["OmikujiTime"]
-        if oldtime == newtime:
-            await interaction.response.send_message(f"QAQ\n可是抽籤系統更新時間本來就是__{newtime}__了阿QQ")
-        else:
-            with open("setting.json","w",encoding="utf8") as setting_file:
-                json.dump(setting,setting_file,indent=4)
-                await interaction.response.send_message(f"已將抽籤系統更新時間從__{oldtime}__調整為__{newtime}__")
+    @app_commands.choices(mod=[app_commands.Choice(name = "check",value = "查閱"),app_commands.Choice(name = "set",value = "設定(需要權限)")])
+    async def check(self, interaction: discord.Interaction,mod: app_commands.Choice[str],hours: Optional[int] = None,minutes: Optional[int] = None):
+        name = interaction.user.mention
+        if mod.name == "check" :
+            with open("setting.json","r",encoding="utf8") as setting_file:
+                setting = json.load(setting_file)
+            id != interaction.user.id
+            await interaction.response.send_message(f'User：{name}\n目前抽籤系統是每天的__{setting["OmikujiTime"]}__更新呦!')
+        elif mod.name == "set":#has_permissions (administrator=True)
+            if "A" != "B":
+                if hours == None:
+                    hours = 0
+                elif hours >= 23:
+                    hours = 23
+                if minutes == None:
+                    minutes = 0
+                elif minutes >= 59 :
+                    minutes = 59
+                oldtime = setting["OmikujiTime"]
+                setting["OmikujiTime"] = f"{hours}:{minutes}"
+                newtime = setting["OmikujiTime"]
+                if oldtime == newtime:
+                    await interaction.response.send_message(f"QAQ\n可是抽籤系統更新時間本來就是__{newtime}__了阿QQ")
+                else:
+                    with open("setting.json","w",encoding="utf8") as setting_file:
+                        json.dump(setting,setting_file,indent=4)
+                        await interaction.response.send_message(f"已將抽籤系統更新時間從__{oldtime}__調整為__{newtime}__")
 
 
 
