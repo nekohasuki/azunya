adc36cb8cac749fded5d89bc2f1012a1390bc3f7
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-10 21:28:09 +0800

    Update setting.py

--- cmds/setting.py
+++ cmds/setting.py
@@ -19,10 +19,10 @@ class Setting(Cog_extension):
     #     self.channel = self.bot.get_channel(channel)
     #     await ctx.send(f"set chammel:{self.channel.mention}")
 #設定抽籤系統更新時間
-    @app_commands.command(name = "setomikujitime", description = "抽籤系統更新時間相關")
-    @app_commands.describe(mod = "模式")
-    @app_commands.describe(hours="輸入小時",minutes="輸入分鐘")
-    @app_commands.choices(mod=[app_commands.Choice(name = "check",value = "查閱"),app_commands.Choice(name = "set",value = "設定(需要權限)")])
+    @app_commands.command(name = "omikujitime", description = "抽籤系統更新時間相關")
+    @app_commands.describe(mod = "模式(set模式需要權限)")
+    @app_commands.describe(hours="輸入小時(查閱模式不用輸入)",minutes="輸入分鐘(查閱模式不用輸入)")
+    @app_commands.choices(mod=[app_commands.Choice(name = "check",value = "check"),app_commands.Choice(name = "set",value = "set)")])
     async def check(self, interaction: discord.Interaction,mod: app_commands.Choice[str],hours: Optional[int] = None,minutes: Optional[int] = None):
         name = interaction.user.mention
         if mod.name == "check" :
@@ -30,8 +30,10 @@ class Setting(Cog_extension):
                 setting = json.load(setting_file)
             id != interaction.user.id
             await interaction.response.send_message(f'User：{name}\n目前抽籤系統是每天的__{setting["OmikujiTime"]}__更新呦!')
-        elif mod.name == "set":#has_permissions (administrator=True)
-            if "A" != "B":
+        elif mod.name == "set":
+            if "A" != "A":#has_permissions (administrator=True)
+                with open("setting.json","r",encoding="utf8") as setting_file:
+                    setting = json.load(setting_file)
                 if hours == None:
                     hours = 0
                 elif hours >= 23:
@@ -49,6 +51,9 @@ class Setting(Cog_extension):
                     with open("setting.json","w",encoding="utf8") as setting_file:
                         json.dump(setting,setting_file,indent=4)
                         await interaction.response.send_message(f"已將抽籤系統更新時間從__{oldtime}__調整為__{newtime}__")
+            else:
+                await interaction.response.send_message(f"User：{name}你沒有權限更改喔!")
+
 
 
 
