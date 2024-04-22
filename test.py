04a657508a2ad0fa016f75843399f8f266fcf67f
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-23 03:21:12 +0800

    Update test.py

--- test.py
+++ test.py
@@ -5,21 +5,24 @@ with open('cmds\\data\\user_data.json','r',encoding='utf8') as UserDataFile:
 
 
 
+userdata={}
+user_id = 938100109240074310
+new_userdata = {f"{user_id}":{"global_name":"","name":"","code":"","top_relo":"","name_card":"","point":{"state":"","now_count":"","history_count":"","consumption":"","give":"","deprivation":""},"trade_count":"","VIP_tickets":"","VIP_chip":""}}
 
 
-# omikuji_update = {"938100109240074310":{"global_name":"","name":"","code":"","top_relo":"","name_card":"","point":{"state":"","now_count":"","history_count":"","consumption":"","give":"","deprivation":""},"trade_count":"","VIP_tickets":"","VIP_chip":""}}
-userdata = {}
-A = ''
-omikuji_update = {"":{123:"","":""}}
-print(A)
-A.append(omikuji_update)
-print(A)
+print(new_userdata[f"{user_id}"]["point"])
+new_userdata = userdata
+print(new_userdata[f"{user_id}"]["point"])
+new_userdata[f"{user_id}"]["point"]["state"] = "56"
+print(new_userdata[f"{user_id}"]["point"])
 
 
-userdata = A
-userdata.update(userdata)
+
+userdata.update(new_userdata)
 with open('cmds\\data\\user_data.json','w',encoding='utf8') as UserDataFile:
     json.dump(userdata,UserDataFile,indent=4)
+
+
 
 
 
