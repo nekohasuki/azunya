106d9a367560769f175e644b5de00f8356f7658a
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-25 18:01:48 +0800

    Update point.py

--- cmds/point.py
+++ cmds/point.py
@@ -77,36 +77,49 @@ class Point(Cog_extension):
     async def givepoint(self , interaction:discord.Integration , user: Optional[str] = None , count: Optional[int] = None):
         if user == None:
             user = f'<@{interaction.user.id}>'
+        if count == None:
+            count = 0
         if "@" in user:
             if "&" not in user:
-                if count == None:
-                    count = 0
                 count = abs(count)
                 userA = interaction.user
-                userB = interaction.guild.get_member(int(user[2:-1]))
+                userB = interaction.guild.get_member(int(user[2:-1]))                       
                 with open('cmds\\data\\user_data.json' , 'r' , encoding='utf8') as UserDataFile:
                     userdata = json.load(UserDataFile)
-                if count <= userdata[f"{userA.id}"]['point']['now_count']:
-                #抓取資料
-                    userdata_updata_A = userdata[f"{userA.id}"]
-                    now_count_A = userdata_updata_A['point']['now_count']
-                    history_count_A = userdata_updata_A['point']['history_count']
-                    userdata_updata_B = userdata[f"{userB.id}"]
-                    now_count_B = userdata_updata_B['point']['now_count']
-                    history_count_B = userdata_updata_B['point']['history_count']
-                #刷入資料
-                    userdata_updata_A['point']['now_count'] = int(now_count_A) - count
-                    userdata_updata_A['point']['history_count'] = int(history_count_A) - count
-                    userdata_updata_B['point']['now_count'] = int(now_count_B) + count
-                    userdata_updata_B['point']['history_count'] = int(history_count_B) + count
-                #更新資料
-                    userdata.update(userdata_updata_A)
-                    userdata.update(userdata_updata_B)
-                    with open('cmds\\data\\user_data.json' , 'w' , encoding='utf8') as UserDataFile:
-                        json.dump(userdata , UserDataFile , indent=4)
-                    await interaction.response.send_message(f'User：{userA.mention}給予了User：{userB.mention}**{count}**點')
+                if userdata[f'{userA.id}']['point']['state'] == str(True):
+                    if userdata[f'{userB.id}']['point']['state'] == str(True) or userdata[f'{userB.id}']['point']['state'] == str(False):
+                        if count <= userdata[f"{userA.id}"]['point']['now_count']:
+                        #抓取資料
+                            userdata_updata_A = userdata[f"{userA.id}"]
+                            now_count_A = userdata_updata_A['point']['now_count']
+                            history_count_A = userdata_updata_A['point']['history_count']
+                            history_count_A = userdata_updata_A['point']['give']
+                            userdata_updata_B = userdata[f"{userB.id}"]
+                            now_count_B = userdata_updata_B['point']['now_count']
+                            history_count_B = userdata_updata_B['point']['history_count']
+                        #刷入資料
+                            userdata_updata_A['point']['now_count'] = int(now_count_A) - count
+                            userdata_updata_A['point']['give'] = int(history_count_A) + count
+                            userdata_updata_B['point']['now_count'] = int(now_count_B) + count
+                            userdata_updata_B['point']['history_count'] = int(history_count_B) + count
+                        #更新資料
+                            userdata[f'{userA.id}'].update(userdata_updata_A)
+                            userdata[f'{userB.id}'].update(userdata_updata_B)
+                            with open('cmds\\data\\user_data.json' , 'w' , encoding='utf8') as UserDataFile:
+                                json.dump(userdata , UserDataFile , indent=4)
+                            await interaction.response.send_message(f'User：{userA.mention}給予了User：{userB.mention}**{count}**點')
+                        else:
+                            await interaction.response.send_message(f'User：{userA.mention}\n你的點數似乎不夠喔')
+                    elif userdata[f'{userB.id}']['point']['state'] == str(None):
+                        await interaction.response.send_message(f'可是User：{userB.mention}\n從未註冊過P卡\n請先讓{userB.mention}回[__領取身分的地方__](https://ptb.discord.com/channels/{interaction.guild.id}/{setting['ROLE_MESSAGE_CHANNEL_ID']}/{setting['ROLE_MESSAGE_ID']})註冊')
+                    else:
+                        await interaction.response.send_message(f'對方用戶資料損毀，請聯絡管理員檢查用戶資料')
+                elif userdata[f'{userA.id}']['point']['state'] == str(False):
+                    await interaction.response.send_message(f'User：{userA.mention}\n你的P卡註冊狀態為收回\n請先回[__領取身分的地方__](https://ptb.discord.com/channels/{interaction.guild.id}/{setting['ROLE_MESSAGE_CHANNEL_ID']}/{setting['ROLE_MESSAGE_ID']})重新註冊')
+                elif userdata[f'{userA.id}']['point']['state'] == str(None):
+                    await interaction.response.send_message(f'可是User：{userA.mention}\n你從未註冊過P卡\n請先回[__領取身分的地方__](https://ptb.discord.com/channels/{interaction.guild.id}/{setting['ROLE_MESSAGE_CHANNEL_ID']}/{setting['ROLE_MESSAGE_ID']})註冊')
                 else:
-                    await interaction.response.send_message(f'User：{userA.mention}\n你的點數似乎不夠喔')
+                    await interaction.response.send_message(f'用戶資料損毀，請聯絡管理員檢查用戶資料')
             else:
                 await interaction.response.send_message(f'這好像是某個身分組並不是某位User')
         else:
@@ -151,6 +164,10 @@ class Point(Cog_extension):
     @app_commands.describe(mod = '模式(需要權限)' , user='輸入用戶' , count='輸入數量' , reason = '輸入原因')
     @app_commands.choices(mod=[app_commands.Choice(name = 'add' , value = 'add') , app_commands.Choice(name = 'remove' , value = 'remove')])
     async def point(self , interaction:discord.Integration , mod: app_commands.Choice[str] , user: Optional[str] = None , count: Optional[int] = None , reason: Optional[str] = None):        
+        if count == None:
+            count = 0
+        if reason == None:
+            reason = '沒什麼特別的原因'
         if user == None:
             await interaction.response.send_message(f'請輸入使用者')
         else:
@@ -159,10 +176,6 @@ class Point(Cog_extension):
             role_list = setting['MOD_roles']
             id = interaction.user.id
             counter = 0
-            if count == None:
-                count = 0
-            if reason == None:
-                reason = '沒什麼特別的原因'
             for role in role_list:
                 if counter == 1:
                     break
@@ -171,43 +184,56 @@ class Point(Cog_extension):
                     counter += 1
                     if "<@" in user:
                         if "&" not in user:
-                            user_id = user[2:-1]
                             user = interaction.guild.get_member(int(user[2:-1]))
                             if interaction.user != user:
                                 if interaction.user.top_role.position >= user.top_role.position:
-                                    count = abs(count)
-                                #添加點數
-                                    if mod.name == "add":
-                                        with open('cmds\\data\\user_data.json' , 'r' , encoding='utf8') as UserDataFile:
-                                            userdata = json.load(UserDataFile)
-                                    #抓取資料
-                                        userdata_updata = userdata[f"{user_id}"]
-                                        now_count = userdata_updata['point']['now_count']
-                                        history_count = userdata_updata['point']['history_count']
-                                    #刷入資料
-                                        userdata_updata['point']['now_count'] = int(now_count) + count
-                                        userdata_updata['point']['history_count'] = int(history_count) + count
-                                    #更新資料
-                                        userdata.update(userdata_updata)
-                                        with open('cmds\\data\\user_data.json' , 'w' , encoding='utf8') as UserDataFile:
-                                            json.dump(userdata , UserDataFile , indent=4)
-                                        await interaction.response.send_message(f'已為User：{user.mention}添加了**{count}**點\n原因：{reason}')
-                                #移除點數                                
-                                    if mod.name == "remove":
-                                        with open('cmds\\data\\user_data.json' , 'r' , encoding='utf8') as UserDataFile:
-                                            userdata = json.load(UserDataFile)
-                                    #抓取資料
-                                        userdata_updata = userdata[f"{user_id}"]
-                                        now_count = userdata_updata['point']['now_count']
-                                        history_count = userdata_updata['point']['history_count']
-                                    #刷入資料
-                                        userdata_updata['point']['now_count'] = int(now_count) - count
-                                        userdata_updata['point']['history_count'] = int(history_count) - count
-                                    #更新資料
-                                        userdata.update(userdata_updata)
-                                        with open('cmds\\data\\user_data.json' , 'w' , encoding='utf8') as UserDataFile:
-                                            json.dump(userdata , UserDataFile , indent=4)
-                                        await interaction.response.send_message(f'已為User：{user.mention}移除了**{count}**點\n原因：{reason}')
+                                    with open('cmds\\data\\user_data.json' , 'r' , encoding='utf8') as UserDataFile:
+                                        userdata = json.load(UserDataFile)
+                                    if userdata[f'{user.id}']['point']['state'] == str(True) or userdata[f'{user.id}']['point']['state'] == str(False):
+                                        count = abs(count)
+                                    #添加點數
+                                        if mod.name == "add":
+                                        #抓取資料
+                                            userdata_updata = userdata[f"{user.id}"]
+                                            now_count = userdata_updata['point']['now_count']
+                                            history_count = userdata_updata['point']['history_count']
+                                        #刷入資料
+                                            userdata_updata['point']['now_count'] = int(now_count) + count
+                                            userdata_updata['point']['history_count'] = int(history_count) + count
+                                        #更新資料
+                                            userdata[f"{user.id}"].update(userdata_updata)
+                                            with open('cmds\\data\\user_data.json' , 'w' , encoding='utf8') as UserDataFile:
+                                                json.dump(userdata , UserDataFile , indent=4)
+                                            await interaction.response.send_message(f'已為User：{user.mention}添加了**{count}**點\n原因：{reason}')
+                                    #移除點數                                
+                                        if mod.name == "remove":
+                                            with open('cmds\\data\\user_data.json' , 'r' , encoding='utf8') as UserDataFile:
+                                                userdata = json.load(UserDataFile)
+                                        #抓取資料
+                                            userdata_updata = userdata[f"{user.id}"]
+                                            now_count = userdata_updata['point']['now_count']
+                                            history_count = userdata_updata['point']['history_count']
+                                        #刷入資料
+                                            userdata_updata['point']['now_count'] = int(now_count) - count
+                                            userdata_updata['point']['history_count'] = int(history_count) - count
+                                        #更新資料
+                                            userdata[f"{user.id}"].update(userdata_updata)
+                                            with open('cmds\\data\\user_data.json' , 'w' , encoding='utf8') as UserDataFile:
+                                                json.dump(userdata , UserDataFile , indent=4)
+                                            await interaction.response.send_message(f'已為User：{user.mention}移除了**{count}**點\n原因：{reason}')
+                                    elif userdata[f'{user.id}']['point']['state'] == str(None):
+                                        await interaction.response.send_message(f'可是User：{user.mention}\n還從未註冊過P卡\n請先讓{user.mention}回[__領取身分的地方__](https://ptb.discord.com/channels/{interaction.guild.id}/{setting['ROLE_MESSAGE_CHANNEL_ID']}/{setting['ROLE_MESSAGE_ID']})註冊')
+                                    else:
+                                        await interaction.response.send_message(f'對方用戶資料損毀，請聯絡管理員檢查用戶資料')
+
+
+
+
+
+
+
+
+
                                 else:
                                     await interaction.response.send_message(f'權限不足')
                             else:
@@ -218,6 +244,7 @@ class Point(Cog_extension):
                             await interaction.response.send_message(f'User：**{interaction.user.global_name}** 請問...\nuser參數裡你放了甚麼??')
             if counter == 0:
                 await interaction.response.send_message(f'沒有權限')
+                
 
 
 
