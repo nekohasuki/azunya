86171a856688cf3b9e2f8ebdf66336c15174be45
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-25 18:01:51 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import json
-with open('cmds\\data\\user_data.json','r',encoding='utf8') as UserDataFile:
+with open('cmds\\data\\test.json','r',encoding='utf8') as UserDataFile:
     userdata = json.load(UserDataFile)
 #抓取檔案用的不要理他
 
@@ -9,14 +9,15 @@ count = 1
 #---------------------------------------------
 
 userdata_updata = userdata[f"{user}"]
+print(userdata)
 now_count = userdata_updata['point']['now_count']
 history_count = userdata_updata['point']['history_count']
 userdata_updata['point']['now_count'] = int(now_count) + count
 userdata_updata['point']['history_count'] = int(history_count) + count
 
 
-userdata.update(userdata_updata)
-with open('cmds\\data\\user_data.json','w',encoding='utf8') as UserDataFile:
+userdata[f"{user}"].update(userdata_updata)
+with open('cmds\\data\\test.json','w',encoding='utf8') as UserDataFile:
     json.dump(userdata,UserDataFile,indent=4)
 
 
