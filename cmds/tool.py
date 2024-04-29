4253d9f8f8d664ad0bf3e5a48b455d8cde924ccf
nekohasuki <nekohasuki.ch@gmail.com>
2024-04-30 01:17:32 +0800

    Update tool.py

--- cmds/tool.py
+++ cmds/tool.py
@@ -82,7 +82,8 @@ class Tool(Cog_extension):
 #以梓喵身分發送訊息
     @commands.command()
     async def say(self,ctx, *,msg):
-        print(msg)
+        nowtime = datetime.datetime.now().strftime('%H:%M')
+        print(f'{nowtime} | {ctx.author.display_name} say:{msg}')
         await ctx.channel.purge(limit=1)
         await ctx.send(msg)
 #身分領取embed
@@ -144,7 +145,7 @@ class Tool(Cog_extension):
 #暫時
     commandname = (f'{prefix}name')
     @app_commands.command(name = commandname, description = '這是一段描述')
-    async def name(self,interaction:discord.Integration):
+    async def name(self,interaction:discord.Interaction):
         # with open('dict.json','r',encoding='utf8') as dict_file:
         #     dict = json.load(dict_file)
         # dict_dessert = dict['dessert']
