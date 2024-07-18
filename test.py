836c23b2e57fcd911a3d53e0a7425e09cc89fd8b
nekohasuki <nekohasuki.ch@gmail.com>
2024-07-19 04:55:35 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
+from PIL import Image
+import os,random
+directory = r'imege\color'
+os.makedirs(directory, exist_ok=True)
+width = 192
+height = 108
 
+R = random.randint(0xaf,0xff)
+G = random.randint(0xaf,0xff)
+B = random.randint(0xaf,0xff)
+file_path = os.path.join(directory, f'0x{R:02x}{G:02x}{B:02x}.png')
+if not os.path.exists(file_path):
+    image = Image.new('RGB', (width, height), (R, G, B))
+    image.save(file_path, format='PNG')
+else:
+    n+=1
+    print(f'第{n}次重複')
+# #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 # #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
