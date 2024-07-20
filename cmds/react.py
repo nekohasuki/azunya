9de6ee8edb50ed69c5e654341cf79c4f89e8dc95
nekohasuki <nekohasuki.ch@gmail.com>
2024-07-20 20:41:34 +0800

    Update react.py

--- cmds/react.py
+++ cmds/react.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 import json
-with open('setting.json','r',encoding='utf8') as setting_file:
+with open('setting.json','r',encoding='utf-8') as setting_file:
     setting = json.load(setting_file)
 
 from core.classes import Cog_extension
