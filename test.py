4835a6994f251d013136cc8c455d5ad2bf614871
nekohasuki <nekohasuki.ch@gmail.com>
2024-08-16 04:38:11 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,65 @@
-import json,random
+from discord import app_commands,SelectOption
+import json,os,random
 # with open(f'cmds/rpg_define/rpg_definitions.json','r',encoding='utf-8') as RPG_definitions_fill: 
 #     rpg_definitions = json.load(RPG_definitions_fill)
 # with open(f'cmds/rpg_define/rpg_definitions.json','w',encoding='utf-8') as RPG_definitions_fill: 
 #     json.dump(rpg_definitions,RPG_definitions_fill,indent=4)
+open_file='''
+with open('setting.json','r',encoding='utf-8') as setting_file:
+    setting = json.load(setting_file)
+with open('cmds/data/user_data.json' ,'r' ,encoding='utf-8') as userdata_file:
+    userdata = json.load(userdata_file)
+with open('cmds/rpg_define/format.json','r',encoding='utf-8') as Format_file:
+    format = json.load(Format_file)
+lang=format[Lang[0]]['additional']
+lang.update(format['Shared']['additional'])
+with open(f'cmds/rpg_define/{Lang[0]}.lang','r',encoding='utf-8') as Lang_file:
+    for line in Lang_file:
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+        key,value = line.split('=',1)
+        if value in format[Lang[0]]["lang"]:
+            lang[key] = format[Lang[0]]["lang"][value]
+        else:
+            lang[key] = f'"{value}"'
+for line in lang:
+    if eval(lang[line]) in format[Lang[0]]['additional']:
+        lang[line] = format[Lang[0]]['additional'][eval(lang[line])]
+
+with open(f'cmds/rpg_define/rpg_definitions.json','r',encoding='utf-8') as RPG_definitions_fill:
+    rpg_definitions = json.load(RPG_definitions_fill)
+'''
+
+Lang = ['zh_TW']
+user = str(938100109240074310)
+
+variable = {}
+exec(open_file,globals(),variable)
+userdata = variable.get('userdata')
+setting = variable.get('setting')
+lang = variable.get('lang')
+rpg_definitions = variable.get('rpg_definitions')
+
+
+
+
+
+Equipping = []
+for line in userdata[user]['RPG']['Equipping']:
+    if userdata[user]['RPG']['Equipping'][line] != {}:
+        Equipping.append(f'{eval(lang.get(line,lang['error402']))}:\n    {'\n    '.join(list(userdata[user]['RPG']['Equipping'][line]))}')
+if Equipping != []:
+    Equipping = '\n'.join(Equipping)
+else:
+    Equipping = eval(lang.get('Maybe_you_should_equip_something?',lang['error402']))
+
+
+
+
+    
+print(Equipping)
+
 
 # if '屬性':
 #     Aa = 100
@@ -14,16 +71,8 @@ import json,random
 #         A = B
 #         B = a
 
-# print(A,B)
-# print(int(random.uniform(A-0.5,B)+.5))
-
-A = {'ABC':123}
-print(list(A)[0])
-
-
-
-
-
+#     print(A,B)
+#     print(int(random.uniform(A-0.5,B)+.5))
 # if '品階':
 #     A = 10
 #     B = 1
@@ -39,90 +88,6 @@ print(list(A)[0])
 
 #     print(int(random.uniform(C+0.5,A)+0.5))
 
-
-
-
-
-
-
-
-
-# from discord import app_commands,SelectOption
-# import json,os
-# # # /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-# if ('此處用於測試時代替真實玩家資料' == "with open('uesr_data.json','r',encoding='utf-8')") == False:
-#     data={
-#         "123": {
-#             "name": "nekoha__suki",
-#             "display_name": "\u8c93\u7fbd",
-#             "global_name": "nekoha__suki",
-#             "RPG":{
-#                 "language":"zh_TW",
-#                 "first_online_time":"2024-00-00 00:00:00.00000000",
-#                 "color":"0x000000",
-#                 "coins":0,
-#                 "Race":"MoonRabbit",
-#                 "EXP":{
-#                     "now":0,
-#                     "max":10
-#                 },
-#                 "Main_profession":{
-#                     "class":"Thief",
-#                     "level":0
-#                 },
-#                 "Sub_profession":{
-#                     "Merchant":1,
-#                     "Pharmacist":1,
-#                     "Jeweler":1,
-#                     "Floriculturist":1,
-#                     "Chef":1,
-#                     "Armourer":1,
-#                     "Hammersmith":0
-#                 },
-#                 "attributes":{
-#                     "HP":0,
-#                     "SP":0,
-#                     "MP":0,
-#                     "ATK":0,
-#                     "MATK":0,
-#                     "DEF":0,
-#                     "MDEF":0,
-#                     "SPD":0,
-#                     "AGI":0,
-#                     "LUK":0,
-#                     "CHR":0,
-#                     "SAN":0
-#                 },
-#                 "item":{
-#                     "Weapon":["Claw"],
-#                     "Weapon_II":[""],
-#                     "Weapon_III":[""],
-#                     "Armor":["Leather_armor"],
-#                     "Runes":[],
-#                     "Flowers":[],
-#                     "Gemstones":[],
-#                     "Cuisine":[]
-#                     },
-#                 "handbook":{
-#                     "Weapon":["Claw"],
-#                     "Weapon_II":[""],
-            
-#                     "Weapon_III":[""],
-#                     "Armor":["Leather_armor"],
-#                     "Runes":[],
-#                     "Flowers":[],
-#                     "Gemstones":[],
-#                     "Cuisine":[],
-#                     "Hostile":[]
-#                 },
-#                 "PVP":{
-#                     "win":0,
-#                     "lose":0,
-#                     "tie":0
-#                 }
-#             }
-#         }
-#     }
 # #--------------------------------------------------------------------------------------------------------
 # if '這裡是自動處理*.ling的代碼':
 #     Lang='ja_JP.lang'
@@ -154,96 +119,6 @@ print(list(A)[0])
 #     print(lang)
 #     #日文要檢查空白符號,因未知原因會自動卡一個空格
 # #--------------------------------------------------------------------------------------------------------
-
-# first_online_time=lang['first_online_time']
-# user_first_online_time=user['RPG']['first_online_time']
-# language=lang['language']
-# user_language=user['RPG']['language']
-# Race = lang['Race']
-# user_Race = lang[user['RPG']['Race']]
-# EXP = lang['EXP']
-# user_EXP = {'max':user['RPG']['EXP']['max'],'now':user['RPG']['EXP']['now']}
-# main_profession = lang['Main_profession']
-# user_main_profession = {f'{lang['Main_profession']}{lang['class']}':lang[user['RPG']['Main_profession']['class']],lang['level']:user['RPG']['Main_profession']['level']}
-# sub_professions = lang['Sub_profession']
-# user_sub_professions = {lang['Merchant']:user['RPG']['Sub_profession']['Merchant'],lang['Pharmacist']:user['RPG']['Sub_profession']['Pharmacist'],lang['Jeweler']:user['RPG']['Sub_profession']['Jeweler'],lang['Floriculturist']:user['RPG']['Sub_profession']['Floriculturist'],lang['Chef']:user['RPG']['Sub_profession']['Chef'],lang['Armourer']:user['RPG']['Sub_profession']['Armourer'],lang['Hammersmith']:user['RPG']['Sub_profession']['Hammersmith']}
-# user_color = user['RPG']['color']
-# color = lang['color']
-# # Character_Sheet = lang['character_sheet']
-# EXP_bar=['/|','/|','/|','/|','/|','.:','.:','.:','.:','.:','.:','.:','.:','.:','.:']
-# EXP_bar=f'{'/|'*int(15/user_EXP['max']*(user_EXP['now']))}{'.:'*(15-int(15/user_EXP['max']*(user_EXP['now'])))}'
-# /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-# print(EXP_bar)
-# import random
-# if '此處用於找出等級最高的副職業' != 1:
-#     top_sub_profession = []
-#     maxlave = []
-#     for sub_profession_1 in user['RPG']['Sub_profession']:
-#         maxlave.append(user['RPG']['Sub_profession'][sub_profession_1])
-#         if user['RPG']['Sub_profession'][sub_profession_1] < max(maxlave):
-#             maxlave.remove(user['RPG']['Sub_profession'][sub_profession_1])
-#         for sub_profession_2 in user['RPG']['Sub_profession']:
-#             if user['RPG']['Sub_profession'][sub_profession_1] > user['RPG']['Sub_profession'][sub_profession_2]:
-#                 if {sub_profession_1:user['RPG']['Sub_profession'][sub_profession_1]} not in top_sub_profession:
-#                     top_sub_profession.append({sub_profession_1:user['RPG']['Sub_profession'][sub_profession_1]})
-#                 if {sub_profession_2:user['RPG']['Sub_profession'][sub_profession_2]}  in top_sub_profession:
-#                     top_sub_profession.remove({sub_profession_2:user['RPG']['Sub_profession'][sub_profession_2]})
-#     if top_sub_profession == []:
-#         top_sub_profession.append({'full_equilibrium':user['RPG']['Sub_profession'][sub_profession_1]})
-#     else:
-#         random.shuffle(top_sub_profession)
-#         maxlave = len(maxlave)-1
-#     top_sub_profession = top_sub_profession[0]
-    # top_sub_profession = {"class":list(top_sub_profession.keys())[0],"level":list(top_sub_profession.values())[0]}
-
-
-
-#embed.add_field(
-# name='',
-# value=f'{'名稱':<3}: **{interaction.user.display_name}**\n
-# lang['profession']: {lang[user['RPG']['Main_profession']['class']]:<5}Lv.{user['RPG']['Main_profession']['level']:<3}
-# {'/':<2}{top_sub_profession['class']:<4}Lv.{top_sub_profession['level']:<5}\n
-# {'種族':<3}: {Race}\n
-# {'EXP':<4}: {EXP['now']} / {EXP['max']}\n
-# `{str(EXP_bar)[1:-1].replace(',','').replace(' ','').replace("'",'')}`'
-# #,inline=False)
-# #embed.add_field(
-# name='__**-------------------------------------**__',
-# value=(f'|!__**H~P**__!| :_`{200:>4}`_\u3000|!__**S!A!N**__!| :_`{0:>4}`_\u3000\n|!__**S~P**__!| :_`{0:>4}`_\u3000|!__**M~P**__!| :_`{0:>4}`_\u3000\n|!__**A!T!K**__!| :_`{0:>4}`_\u3000|!__**MATK**__!| :_`{0:>4}`_\u3000\n|!__**D!E!F**__!| :_`{0:>4}`_\u3000|!__**MDEF**__!| :_`{0:>4}`_\u3000\n|!__**L!U!K**__!| :_`{0:>4}`_\u3000|!__**S!P!D**__!| :_`{0:>4}`_\u3000\n|!__**A!G!\u200BI\u2009**__!| :_`{0:>4}`_\u3000|!__**C!H!R**__!| :_`{0:>4}`_\u3000').replace('~','\u2009\u3000').replace('!','\u200A\u2004').replace(' ','\u2007\u200A')
-#,inline=False)
-                
-
-# print(user['RPG']['Main_profession']['level'])
-
-
-# from PIL import Image
-# import os,random,shutil,asyncio
-# directory = r'imege\rpg\color'
-# shutil.rmtree(directory)
-# os.makedirs(directory, exist_ok=True)
-# width = 192
-# height = 108
-
-# async def tset():
-#     listdir=os.listdir(directory)
-#     for A in range(0x000000,0xffffff+1):
-#         A1=(f'0x{(A):06x}.png')
-#         if A1 not in listdir:
-#             R = (A >> 16) & 0xff
-#             G = (A >> 8) & 0xff
-#             B = A & 0xff
-#             file_path = os.path.join(directory, f'0x{R:02x}{G:02x}{B:02x}.png')
-#             if not os.path.exists(file_path):
-#                 image = Image.new('RGB', (width, height), (R, G, B))
-#                 image.save(file_path, format='PNG')
-#                 print(f'0x{R:02x}{G:02x}{B:02x}')
-#             else:
-#                 print(f'0x{R:02x}{G:02x}{B:02x}(重複)')
-
-# asyncio.run(tset())
-
-# #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-# #///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
